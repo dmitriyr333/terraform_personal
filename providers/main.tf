aa751d1fe8cafa068e597fd99476c9e2e234efcf
Dmitriy Rozentsvay <dmitriyr333@gmail.com>
2017-08-07 15:49:52 -0400

    added sg

--- providers/main.tf
+++ providers/main.tf
@@ -33,21 +33,21 @@ resource "aws_eip" "ip" {
   instance = "${module.create_instances.publicIP}"
 }
 
-# resource "aws_security_group" "allow_ssh" {
-#   name        = "allow_ssh"
-#   description = "allow ssh"
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "allow ssh"
 
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags {
-#     Name = "allow_ssh"
-#   }
-# }
+  tags {
+    Name = "allow_ssh"
+  }
+}
 
 
 # # outputs
